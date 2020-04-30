d100c79f4c81d3067e87bc5bf273001f354e87a2
Caroline <caroline.portilla@gmail.com>
2020-04-30 13:49:18 +0800

    beginnings of apiRoutes.js, need to review fs for beter understanding

---
+++ Develop/public/assets/js/apiRoutes.js
@@ -0,0 +1,15 @@
+module.exports = function(app) {
+   /*
+    app.get("/api/notes", function(req, res) {
+      res.json();
+    });
+ 
+    app.post("/api/notes", function(req, res) {
+      
+    });
+  
+    app.delete("/api/notes/:id", function(req, res) {
+      
+    });*/
+  };
+  
