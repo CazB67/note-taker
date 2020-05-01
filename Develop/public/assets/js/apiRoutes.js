0b49f4e9ca3c81764809390c8ecf5c4ae9cc9122
Caroline <caroline.portilla@gmail.com>
2020-05-01 12:56:15 +0800

    fixed links so finally reading javascript

--- Develop/public/assets/js/apiRoutes.js
+++ Develop/public/assets/js/apiRoutes.js
@@ -1,13 +1,17 @@
+const notesData = require("../../../db/db.json");
+
 module.exports = function(app) {
-   /*
+   
     app.get("/api/notes", function(req, res) {
-      res.json();
+      res.json(notesData);
     });
  
     app.post("/api/notes", function(req, res) {
-      
+      if (notesData === "") {
+        notesData.push(req.body)
+      }
     });
-  
+  /*
     app.delete("/api/notes/:id", function(req, res) {
       
     });*/
--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -5,6 +5,10 @@ const express = require("express");
 // Tells node that we are creating an "express" server
 const app = express();
 
+
+// Use This to enable Static Stuff --> ie javascript and css. 
+app.use(express.static('public'))
+
 // Sets an initial port. We"ll use this later in our listener
 const PORT = process.env.PORT || 8080;
 
@@ -13,10 +17,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
+
+
 // The routes below point our server to a series of "route" files.
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs
 
-//require("./apiRoutes")(app);
+require("./apiRoutes")(app);
 require("./htmlRoutes")(app);
 
 // =============================================================================
